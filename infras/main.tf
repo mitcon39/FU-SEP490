941e767115309d20a504b02389c6b70c175d488c
Nguyen Le <lnguyennb@gmail.com>
2025-01-01 01:37:17 +0700

    update

--- infras/main.tf
+++ infras/main.tf
@@ -23,9 +23,10 @@ resource "google_cloud_run_v2_service" "llm-service" {
   location            = var.location
   deletion_protection = false
   ingress             = "INGRESS_TRAFFIC_ALL"
-  invoker_iam_disabled = false
+  # invoker_iam_disabled = false
 
   template {
+    execution_environment = "EXECUTION_ENVIRONMENT_GEN2"
     containers {
       image = "lnguyennb/node-llm-server"
       ports {
@@ -34,14 +35,14 @@ resource "google_cloud_run_v2_service" "llm-service" {
       resources {
         limits = {
           cpu    = "8"
-          memory = "16Gi"
+          memory = "8Gi"
         }
       }
       startup_probe {
-        initial_delay_seconds = 5
-        timeout_seconds       = 1
+        initial_delay_seconds = 10
+        timeout_seconds       = 3
         period_seconds        = 3
-        failure_threshold     = 1
+        failure_threshold     = 5
         tcp_socket {
           port = 6969
         }
